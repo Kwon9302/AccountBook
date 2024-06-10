96da2043377a12ad9a188b841ee220638e41f8bd
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-06-10 20:20:47 +0900

    가계부 기본 기능 구현(필터x)

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HomeView from "./components/HomeView.vue";
+</script>
+
+<template>
+    <div>
+        <HomeView />
+        <!-- <nav><<router-link to="/"></router-link></nav>
+        <RouterView /> -->
+    </div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+// import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+const pinia = createPinia();
+// Vue.prototype.$http = axios;
+
+app.use(pinia);
+app.use(router);
+
+app.mount("#app");
