39dbe384e1c9db1ef1696d4852cc3ab674d6c243
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-06-12 11:33:39 +0900

    revise1

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -8,81 +8,89 @@
             </div>
             <!-- 날짜 입력 -->
             <form @submit.prevent="handleSubmit">
- 
-                    <h3>날짜</h3>
+                <h3>날짜</h3>
 
-                        <div class="dateInput">
-                            <input
-                                type="number"
-                                v-model="year"
-                                placeholder="YYYY"
-                                required
-                            />
-                            <input
-                                type="number"
-                                v-model="month"
-                                placeholder="MM"
-                                required
-                            />
-                            <input
-                                type="number"
-                                v-model="day"
-                                placeholder="DD"
-                                required
-                            />
-                        </div>
-
+                <div class="dateInput">
+                    <input
+                        type="number"
+                        v-model="year"
+                        placeholder="YYYY"
+                        required
+                    />
+                    <input
+                        type="number"
+                        v-model="month"
+                        placeholder="MM"
+                        required
+                    />
+                    <input
+                        type="number"
+                        v-model="day"
+                        placeholder="DD"
+                        required
+                    />
+                </div>
 
                 <!-- 내용(입출금 선택) -->
-                
-                    <h3>내용</h3>
-                    <div class="selectInOut">
-                        <button
-                            type="button"
-                            @click="selectDeposit"
-                            :class="{ active: isDeposit}"
+
+                <h3>내용</h3>
+                <div class="selectInOut">
+                    <button
+                        type="button"
+                        @click="selectDeposit"
+                        :class="{ active: isDeposit }"
+                    >
+                        들어온 돈
+                    </button>
+                    <button
+                        type="button"
+                        @click="selectWithdraw"
+                        :class="{ active: !isDeposit }"
+                    >
+                        나간 돈
+                    </button>
+                </div>
+                <!-- 숨겨진 select 박스 category -->
+                <div v-if="showModal" class="modal">
+                    <div class="modal-content">
+                        <span class="close" @click="closeModal">&times;</span>
+                        <select
+                            v-if="isDeposit"
+                            v-model="category"
+                            class="dropdown"
                         >
-                            들어온 돈
-                        </button>
-                        <button
-                            type="button"
-                            @click="selectWithdraw"
-                            :class="{ active: !isDeposit}"
-                        >
-                            나간 돈
-                        </button>
-                    </div>
-                    <!-- 숨겨진 select 박스 -->
-                    <div>
-                        <select v-if="isDeposit" v-model="category" class="dropdown">
                             <option value="월급">월급</option>
                             <option value="용돈">용돈</option>
                             <option value="구걸">구걸</option>
                         </select>
-                    </div>
-                    <div>
-                        <select v-if="!isDeposit" v-model="category" class="dropdown">
+
+                        <select
+                            v-if="!isDeposit"
+                            v-model="category"
+                            class="dropdown"
+                        >
                             <option value="공과금">공과금</option>
                             <option value="병원비">병원비</option>
                             <option value="식비">식비</option>
                         </select>
                     </div>
-                    <!-- 내용입력 -->
-                    <div>
-                        <input
+                </div>
+                <!-- 내용입력 -->
+                <div>
+                    <input
                         class="memo"
-                            type="textarea"
-                            v-model="memo"
-                            placeholder="메모내용"
-                        />
-                    </div>
+                        type="textarea"
+                        v-model="memo"
+                        placeholder="메모내용"
+                    />
+                </div>
 
                 <!-- <hr /> -->
-                
-                    <h3>금액</h3>
-                    <div>
+
+                <h3>금액</h3>
+                <div>
                     <input
-                    class="inputMoney"
+                        class="inputMoney"
                         type="number"
                         v-model="amount"
                         placeholder="금액을 입력하세요"
@@ -91,9 +99,13 @@
                 </div>
                 <div class="submitCancel">
                     <button type="submit">입력하기</button>
-                    <button type="button" @click="resetForm" class="cancel-link">
+                    <button
+                        type="button"
+                        @click="resetForm"
+                        class="cancel-link"
+                    >
                         <!-- <router-link to="/list" @click.native="resetForm" class="cancel-link"> -->
-                            취소
+                        취소
                         <!-- </router-link> -->
                     </button>
                 </div>
@@ -108,7 +120,7 @@
 import { useMoneyManageStore } from "@/stores/counter";
 import { ref, onMounted, computed } from "vue";
 // import axios from "axios";
-
+const showModal = ref(false);
 const year = ref("");
 const month = ref("");
 const day = ref("");
@@ -117,9 +129,6 @@ const memo = ref("");
 const category = ref("");
 const isDeposit = ref(null);
 
-const newAmount = computed((amount) => {
-    return amount.toLocalString();
-});
 const moneyManageStore = useMoneyManageStore();
 onMounted(() => {
     moneyManageStore.fetchMoneyManageList();
@@ -127,10 +136,16 @@ onMounted(() => {
 
 const selectDeposit = () => {
     isDeposit.value = true;
+    showModal.value = true;
 };
 
 const selectWithdraw = () => {
     isDeposit.value = false;
+    showModal.value = true;
+};
+
+const closeModal = () => {
+    showModal.value = false;
 };
 const { states, fetchMoneyManageList, saveMoney } = moneyManageStore;
 
@@ -182,13 +197,15 @@ const resetForm = () => {
 
 <style>
 /* 새로운거래 , 날짜, 내용, 금액 글자 margin값 적용 */
-h3{
+h3 {
     margin-bottom: 10px;
     margin-top: 10px;
 }
 
 /* input태그, button 태그 일괄 적용 CSS */
-input, button,select {
+input,
+button,
+select {
     border: 1.090000033378601px solid #6d6d6d;
     border-radius: 10px;
     background-color: #f8eba0;
@@ -198,9 +215,8 @@ input, button,select {
 
 /* 날짜 기입 CSS */
 .dateInput {
-    
     display: flex;
-    justify-content:space-between;  /* 간격 유지 */
+    justify-content: space-between; /* 간격 유지 */
     box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
 }
 
@@ -210,7 +226,7 @@ input, button,select {
     height: 40px;
     /* padding: 5px; */
     border: 1px solid #6d6d6d;
-    
+
     background-color: #f8eba0;
     font-family: Inter;
     font-style: normal;
@@ -224,14 +240,13 @@ input, button,select {
 /* 들어온돈 나간돈 CSS */
 .selectInOut {
     display: flex;
-    justify-content:space-between;
+    justify-content: space-between;
     margin-bottom: 10px;
 }
-.selectInOut button{
+.selectInOut button {
     width: 130px;
     height: 40px;
 }
-
 
 /* 카테고리 선택 CSS */
 .dropdown {
@@ -243,7 +258,7 @@ input, button,select {
     background: #f8eba0;
 
     border: 1px solid #6d6d6d;
-    
+
     margin-bottom: 10px;
 }
 
@@ -251,17 +266,16 @@ input, button,select {
 .memo {
     width: 98%;
     display: flex;
-    justify-content:flex;
+    justify-content: flex;
     height: 50px;
     /* padding: 12px 16px; */
     background-color: #f8eba0;
-    
 }
 
 /* 금액입력 버튼 CSS */
 .inputMoney {
     width: 98%;
-    height: 40px; 
+    height: 40px;
     /* flex: 1; */
     min-width: 50px;
     font-family: Inter;
@@ -274,18 +288,15 @@ input, button,select {
     margin-bottom: 10px;
 }
 
-
 /* 확인 및 취소 버튼 CSS */
 .submitCancel {
-    display: flex; 
-    justify-content:space-between;
-
-
+    display: flex;
+    justify-content: space-between;
 }
 .submitCancel button {
     width: 130px;
     height: 40px;
-    background-color:#af8f6f ;
+    background-color: #af8f6f;
     color: white;
     padding: 10px 20px;
     cursor: pointer;
@@ -294,7 +305,7 @@ input, button,select {
 /* 들어온돈 나간돈 활성 CSS */
 button.active {
     background-color: #af8f6f; /* 활성 상태 CSS */
-    color: black;
+    color: white;
 }
 
 /* input type="number" 화살표 제거 */
@@ -309,5 +320,4 @@ input::-webkit-inner-spin-button {
     text-decoration: none; /* 방문한 링크와 방문하지 않은 링크의 밑줄 제거 */
     color: white; /* 링크의 색상 변경 */
 }
-
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    plugins: [vue()],
+    server: {
+        host: "0.0.0.0", // 모든 네트워크 인터페이스에서 접근 가능
+        port: 5173, // 포트 번호
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+});
